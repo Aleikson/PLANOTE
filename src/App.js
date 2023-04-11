6b393343cbd78dac248f60357e9878a605efb3d5
aleikson <aleiksonsilva@hotmail.com>
2023-04-10 23:15:30 -0300

    added color palette

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Global } from "./components/noteFiles/Global"
+import { PalleteColor } from './components/noteFiles/PalleteColor';
 
 function App() {
   return (
     <div className="App">
       <Global />
+      <PalleteColor />
     </div>
   );
 }
