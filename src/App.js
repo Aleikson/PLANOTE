ca968a0bef551be5c3f0fec98ed43d38dd903988
aleikson <aleiksonsilva@hotmail.com>
2023-04-12 18:39:23 -0300

    Removed: PaletteColord

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { Global } from "./components/noteFiles/Global"
-import { PalleteColor } from './components/noteFiles/PalleteColor';
 
 function App() {
   return (
     <div className="App">
-      <Global />
-      <PalleteColor />
+      <Global />     
     </div>
   );
 }
