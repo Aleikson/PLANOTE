3d4d54c5b07295e44b7e76375fae17124fa1ae36
aleikson <aleiksonsilva@hotmail.com>
2023-04-12 18:48:17 -0300

    Implemented PaletteColor

--- src/components/noteFiles/DeleteNote.js
+++ src/components/noteFiles/DeleteNote.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from "../noteFiles/DeleteNote.module.css"
+import { PaletteColor } from './PaletteColor';
 
 export const DeleteNote = ({ notes = [], deleteNote }) => {
+  const limitedNotes = notes.slice(0, 8);
+  const [noteBgColor, setNoteBgColor] = useState('#fff');
+
+  const handleNoteColorChange = (color) => {
+    setNoteBgColor(color);
+  };
+
   return (
-    <>
-      {notes.map((note) => (
+    <div className={styles.noteContainer}>
+      {limitedNotes.map((note) => (
         <div
           className={styles.note}
-          style={{ transform: `rotate(${note.rotate}deg)` }}
-          key={note.id}
-          draggable="true"
-          onDragEnd={(e) => {
-            e.target.style.left = `${e.pageX - 50}px`;
-            e.target.style.top = `${e.pageY - 50}px`;
+          style={{
+            transform: `rotate(${note.rotate}deg)`,
+            gridColumn: note.col,
+            gridRow: note.row,
+            backgroundColor: noteBgColor,
           }}
+          key={note.id}
         >
           <h2 className={styles.text}>{note.text}</h2>
           <button className={styles.deleteBtn} onClick={() => deleteNote(note.id)}>
             X
           </button>
+          <PaletteColor onChange={handleNoteColorChange} />
         </div>
       ))}
-    </>
+    </div>
   );
-}
+};
