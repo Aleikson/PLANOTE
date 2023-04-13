ca77ea3733ef7bc2c65afb325456e5b2e4557329
aleikson <aleiksonsilva@hotmail.com>
2023-04-13 19:10:45 -0300

    Added handleClick to show color palettes

--- src/components/noteFiles/DeleteNote.js
+++ src/components/noteFiles/DeleteNote.js
@@ -3,33 +3,52 @@ import styles from "../noteFiles/DeleteNote.module.css"
 import { PaletteColor } from './PaletteColor';
 
 export const DeleteNote = ({ notes = [], deleteNote }) => {
-  const limitedNotes = notes.slice(0, 8);
-  const [noteBgColor, setNoteBgColor] = useState('#fff');
+  const limitedNotes = notes.slice(0, 12);
+  const [selectedNoteId, setSelectedNoteId] = useState(null);
 
-  const handleNoteColorChange = (color) => {
-    setNoteBgColor(color);
-  };
+  const handleNoteClick = (noteId) => {
+    setSelectedNoteId(selectedNoteId === noteId ? null : noteId);
+  }
 
   return (
     <div className={styles.noteContainer}>
       {limitedNotes.map((note) => (
-        <div
-          className={styles.note}
-          style={{
-            transform: `rotate(${note.rotate}deg)`,
-            gridColumn: note.col,
-            gridRow: note.row,
-            backgroundColor: noteBgColor,
-          }}
+        <Note
           key={note.id}
-        >
-          <h2 className={styles.text}>{note.text}</h2>
-          <button className={styles.deleteBtn} onClick={() => deleteNote(note.id)}>
-            X
-          </button>
-          <PaletteColor onChange={handleNoteColorChange} />
-        </div>
+          note={note}
+          isSelected={selectedNoteId === note.id}
+          handleNoteClick={handleNoteClick}
+          deleteNote={deleteNote}
+        />
       ))}
+    </div>
+  );
+};
+
+const Note = ({ note, isSelected, handleNoteClick, deleteNote }) => {
+  const [noteBgColor, setNoteBgColor] = useState(note.bgColor || '#fcff82');
+
+  const handleNoteColorChange = (color) => {
+    setNoteBgColor(color);
+    note.bgColor = color;
+  };
+
+  return (
+    <div
+      className={styles.note}
+      style={{
+        transform: `rotate(${note.rotate}deg)`,
+        gridColumn: note.col,
+        gridRow: note.row,
+        backgroundColor: noteBgColor,
+      }}
+      onClick={() => handleNoteClick(note.id)}
+    >
+      <h2 className={styles.text}>{note.text}</h2>
+      <button className={styles.deleteBtn} onClick={() => deleteNote(note.id)}>
+        X
+      </button>
+      {isSelected && <PaletteColor onChange={handleNoteColorChange} />}
     </div>
   );
 };
