72dccfbe388d126ebdc0662f5a63153c5c7bbfaa
aleikson <aleiksonsilva@hotmail.com>
2023-04-17 22:59:28 -0300

    Firebase added

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
