3a25a7995444c6c81f62a9e570b61712c407764f
aleikson <aleiksonsilva@hotmail.com>
2023-04-18 18:38:08 -0300

    Added Grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { Global } from "./components/noteFiles/Global"
 import { GlobalTodo } from './components/TodoFiles/GlobalTodo';
+
 function App() {
   return (
     <div className="App">
-      <Global />
-      <GlobalTodo />     
+      <div className="left">
+        <Global />
+      </div>
+      <div className="right">
+        <GlobalTodo />
+      </div>     
     </div>
   );
 }
