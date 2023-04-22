9d38a908fb78b80de05a470dcdd305e1eacd0ab9
aleikson <aleiksonsilva@hotmail.com>
2023-04-22 18:14:48 -0300

    Changes in the import of components to be individually

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import React from 'react';
 import './App.css';
 import { Global } from "./components/noteFiles/Global"
+import { AddNote } from './components/noteFiles/AddNote';
+import { DeleteNote } from './components/noteFiles/DeleteNote';
 import { GlobalTodo } from './components/TodoFiles/GlobalTodo';
+import { AddTodo } from './components/TodoFiles/AddTodo'
+import { TodoList } from './components/TodoFiles/TodoList'
 
 function App() {
+  const { addNote, deleteNote, notesState } = Global();
+  const { createTodo, input, setInput, todos, toggleComplete, deleteTodo } = GlobalTodo();
+
   return (
     <div className="App">
       <div className="left">
-        <Global />
+        <div className="inputs">
+          <AddNote addNote={addNote} />
+          <AddTodo createTodo={createTodo} input={input} setInput={setInput} />
+        </div>
+        <div className="box">BOX</div>
       </div>
       <div className="right">
-        <GlobalTodo />
-      </div>     
+        <DeleteNote notes={notesState.notes} deleteNote={deleteNote} />
+        <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+      </div>
     </div>
   );
 }
