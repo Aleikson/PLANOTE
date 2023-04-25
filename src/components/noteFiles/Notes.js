ee82b5f68c6162e893934e23ec0b4603002a24cd
aleikson <aleiksonsilva@hotmail.com>
2023-04-25 18:08:07 -0300

    Renamed

---
+++ src/components/noteFiles/Notes.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import styles from "../noteFiles/Notes.module.css"
+import { PaletteColor } from './PaletteColor';
+
+export const Notes = ({ notes = [], deleteNote }) => {
+  const limitedNotes = notes.slice(0, 5);
+  const [selectedNoteId, setSelectedNoteId] = useState(null);
+
+  const handleNoteClick = (noteId) => {
+    setSelectedNoteId(selectedNoteId === noteId ? null : noteId);
+  }
+
+  return (
+    <div className={styles.noteContainer}>
+      {limitedNotes.map((note) => (
+        <Note
+          key={note.id}
+          note={note}
+          isSelected={selectedNoteId === note.id}
+          handleNoteClick={handleNoteClick}
+          deleteNote={deleteNote}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Note = ({ note, isSelected, handleNoteClick, deleteNote }) => {
+  const [noteBgColor, setNoteBgColor] = useState(note.bgColor || '#fcff82');
+
+  const handleNoteColorChange = (color) => {
+    setNoteBgColor(color);
+    note.bgColor = color;
+  };
+
+  return (
+    <div
+      className={styles.note}
+      style={{
+        transform: `rotate(${note.rotate}deg)`,
+        gridColumn: note.col,
+        gridRow: note.row,
+        backgroundColor: noteBgColor,
+      }}
+      onClick={() => handleNoteClick(note.id)}
+    >
+      <h2 className={styles.text}>{note.text}</h2>
+      <button className={styles.deleteBtn} onClick={() => deleteNote(note.id)}>
+        X
+      </button>
+      {isSelected && <PaletteColor onChange={handleNoteColorChange} />}
+    </div>
+  );
+};
