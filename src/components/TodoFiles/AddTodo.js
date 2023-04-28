61dfda878d3ed683f3e3c8c227ff9cde1209767e
aleikson <aleiksonsilva@hotmail.com>
2023-04-28 16:07:56 -0300

    Added: React Modal

--- src/components/TodoFiles/AddTodo.js
+++ src/components/TodoFiles/AddTodo.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import styles from "./GlobalTodo.module.css";
 import { AiOutlinePlus } from 'react-icons/ai';
 
+Modal.setAppElement('#root');
+
 export const AddTodo = ({ createTodo, input, setInput }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTodo();
+    closeModal();
+  }
+
   return (
     <div className={styles.container}>
-      <h2>Add Todo</h2>
-      <form onSubmit={createTodo} className={styles.form}>
-        <textarea
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type='text'
-          placeholder='Add Todo'
-        />
-        <button className={styles.button}>
-          <AiOutlinePlus size={30} />
-        </button>
-      </form>
+      <h2>Todo</h2>
+      <button onClick={openModal}>
+        <AiOutlinePlus size={30} />
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Add Todo"
+        className={styles.modal}
+      >
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+            placeholder='Add Todo'
+          />
+          <button className={styles.button}>
+            Add Todo
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 };
--- src/components/noteFiles/AddNote.js
+++ src/components/noteFiles/AddNote.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import styles from "../noteFiles/AddNote.module.css"
 import { AiOutlinePlus } from 'react-icons/ai';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export const AddNote = ({ addNote }) => {
   const [noteText, setNoteText] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,21 +25,33 @@ export const AddNote = ({ addNote }) => {
 
     addNote(newNote);
     setNoteText('');
+    setModalOpen(false);
   };
 
   return (
     <div className={styles.container}>
-      <h2>Add Notes</h2>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <textarea maxLength={125}
-          placeholder="Add Note"
-          value={noteText}
-          onChange={(e) => setNoteText(e.target.value)}
-        ></textarea>
-        <button className={styles.button}>
-          <AiOutlinePlus size={30} />
-        </button>
-      </form>
+      <h2>Notes</h2>
+      <button onClick={() => setModalOpen(true)}>
+        <AiOutlinePlus size={30} />
+      </button>
+
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+        contentLabel="Add Note"
+        className={styles.modal}
+      >
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <textarea maxLength={125}
+            placeholder="Add Note"
+            value={noteText}
+            onChange={(e) => setNoteText(e.target.value)}
+          ></textarea>
+          <button type="submit" className={styles.button}>
+            Add
+          </button>
+        </form>
+      </Modal>
     </div>
   );
-}
+};
