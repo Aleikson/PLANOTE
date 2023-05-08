34042cf3b3d8c0f5ab1ec64e3a253dbfaa2b5e53
aleikson <aleiksonsilva@hotmail.com>
2023-05-08 16:05:43 -0300

    Added: Delete Modal

--- src/components/noteFiles/Notes.js
+++ src/components/noteFiles/Notes.js
@@ -5,9 +5,16 @@ import { PaletteColor } from './PaletteColor';
 export const Notes = ({ notes = [], deleteNote }) => {
   const limitedNotes = notes.slice(0, 5);
   const [selectedNoteId, setSelectedNoteId] = useState(null);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
   const handleNoteClick = (noteId) => {
     setSelectedNoteId(selectedNoteId === noteId ? null : noteId);
+  }
+
+  const handleDeleteConfirmation = () => {
+    deleteNote(selectedNoteId);
+    setShowDeleteConfirmation(false);
+    setSelectedNoteId(null);
   }
 
   return (
@@ -18,20 +25,31 @@ export const Notes = ({ notes = [], deleteNote }) => {
           note={note}
           isSelected={selectedNoteId === note.id}
           handleNoteClick={handleNoteClick}
-          deleteNote={deleteNote}
+          setShowDeleteConfirmation={setShowDeleteConfirmation}
         />
       ))}
+      {showDeleteConfirmation && (
+        <Modal show={showDeleteConfirmation} onClose={() => setShowDeleteConfirmation(false)}>
+          <p>Do you really want to delete this note?</p>
+          <button onClick={handleDeleteConfirmation}>Yes</button>
+          <button onClick={() => setShowDeleteConfirmation(false)}>No</button>
+        </Modal>
+      )}
     </div>
   );
 };
 
-const Note = ({ note, isSelected, handleNoteClick, deleteNote }) => {
+const Note = ({ note, isSelected, handleNoteClick, setShowDeleteConfirmation }) => {
   const [noteBgColor, setNoteBgColor] = useState(note.bgColor || '#fcff82');
 
   const handleNoteColorChange = (color) => {
     setNoteBgColor(color);
     note.bgColor = color;
   };
+
+  const handleDeleteNote = () => {
+    setShowDeleteConfirmation(true);
+  }
 
   return (
     <div
@@ -45,10 +63,20 @@ const Note = ({ note, isSelected, handleNoteClick, deleteNote }) => {
       onClick={() => handleNoteClick(note.id)}
     >
       <h2 className={styles.text}>{note.text}</h2>
-      <button className={styles.deleteBtn} onClick={() => deleteNote(note.id)}>
+      <button className={styles.deleteBtn} onClick={handleDeleteNote}>
         X
       </button>
       {isSelected && <PaletteColor onChange={handleNoteColorChange} />}
+    </div>
+  );
+};
+
+const Modal = ({ children, onClose }) => {
+  return (
+    <div className={styles.modal} onClick={onClose}>
+      <div className={styles["modal-content"]} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
     </div>
   );
 };
