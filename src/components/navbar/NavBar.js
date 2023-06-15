efe5681552845c05366090560c0a755bbbf04739
aleikson <aleiksonsilva@hotmail.com>
2023-06-15 19:23:40 -0300

    Small changes

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Style from './Navbar.module.css'
-import { FcTodoList } from 'react-icons/fc';
 
 const NavBar = () => {
   return (
     <div className={Style.container}>
       <div className={Style.links}>
-        <FcTodoList className={Style.icon} />
         <div className={Style.linksInner}>
-          <Link to="/" className={Style.link}>Notes</Link>
-          <Link to="todo" className={Style.link}>List</Link>
+          <Link to="/" className={Style.link}>Daily Notes</Link>
+          <Link to="todo" className={Style.link}>My Tasks</Link>
         </div>
       </div>
     </div>
